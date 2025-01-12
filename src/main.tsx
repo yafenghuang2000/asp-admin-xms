e775f5d57794b4f783caccc473b62271ab7a5e89
yafenghuang2000 <194236933+yafenghuang2000@users.noreply.github.com>
2025-01-12 19:01:57 +0800

    refactor: 简化App组件，移除CSS文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import {createRoot} from "react-dom/client"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from "./App";
+
+const container = document.getElementById("root") as HTMLElement
+const root = createRoot(container);
+root.render(
+<React.StrictMode>
+  <App/>
+</React.StrictMode>
 )
