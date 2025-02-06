0229f67951710340f71dc75a3061c99aa65f430e
yafenghuang2000 <yafenghuang2000@users.noreply.github.com>
2025-02-06 12:29:50 +0000

    deploy: 6d72f9185459191857c23ce3fb9c27f2fe33f9cc

---
+++ js/app/index.BH8sfJwN.js
@@ -0,0 +1,2 @@
+import{j as o}from"../vendor/react-vendor.CgBbqpF9.js";import"../vendor/vendor.DhwtFZar.js";import"../vendor/antd-vendor.CK2aOM0R.js";const r=()=>o.jsx("div",{children:"NotFoundPage"});export{r as default};
+//# sourceMappingURL=index.BH8sfJwN.js.map
