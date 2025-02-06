390a614a150058d7913b04df178d67e7555bdb68
yafenghuang2000 <194236933+yafenghuang2000@users.noreply.github.com>
2025-02-06 17:57:28 +0800

    feat(deploy): Add Nginx installation and configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig(({ mode }) => {
         logger: true, // 打印日志
       }),
     ],
-    base: '/asp-admin-xms/', // 部署到 GitHub Pages 上
+    base: '/',
     server: {
       port: 8000,
       open: true,
