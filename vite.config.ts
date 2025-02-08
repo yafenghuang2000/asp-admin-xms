34323387d5a495edff5fc6c818e9c5b06c74b567
yafenghuang2000 <194236933+yafenghuang2000@users.noreply.github.com>
2025-02-08 13:29:45 +0800

    refactor(vite.config): 修改base路径为/asp-admin-xms/

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig(({ mode }) => {
         logger: true, // 打印日志
       }),
     ],
-    base: '/',
+    base: '/asp-admin-xms/',
     server: {
       port: 8000,
       open: true,
